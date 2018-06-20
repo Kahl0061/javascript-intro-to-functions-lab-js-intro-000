5960c9a9cf9518554ce0cac9c8d95c47031fc844
Robbie Kahl <rob.kahl@target.com>
2018-06-20 14:08:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string){
   if (string.toLowerCase === string)
   return "I can't hear you!"
 
-  if (string.Uppercase === string)
+  if (string.toUppercase === string)
   return "YES INDEED"
 
   if(string === "I love you, Grandma")
