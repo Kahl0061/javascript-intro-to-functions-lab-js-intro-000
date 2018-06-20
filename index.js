91bf07c017da4107a503088afb755160cbe702c6
Robbie Kahl <rob.kahl@target.com>
2018-06-20 13:58:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function logwhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if string.toLowerCase === lowercase
-  return "I can't hear you!"
+  if string.toLowerCase === string
+    return "I can't hear you!"
 }
